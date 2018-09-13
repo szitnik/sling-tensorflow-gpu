de19c81257beed932b158799f558b8fda096dad0
Slavko <slavko@zitnik.si>
2018-09-13 21:36:46 +0200

    Test updated

--- test.py
+++ test.py
@@ -3,7 +3,14 @@ import tensorflow as tf
 from tensorflow.python.client import device_lib
 
 def get_available_gpus():
-    local_device_protos = device_lib.list_local_devices()
+
+    # Tensorflow porabi vse resurse, ki so na voljo, ne toliko, kolikor rabi oziroma kar je alocirano prek upravljavca nalog.
+    # To popravimo tako, da ze pri vzpostavitvi seje dolocimo, da se rama ne alocira vnaprej:
+    config = tf.ConfigProto()
+    config.gpu_options.allow_growth = True
+    
+
+    local_device_protos = device_lib.list_local_devices(config)
     return [x.name for x in local_device_protos if x.device_type == 'GPU']
 
 get_available_gpus()
