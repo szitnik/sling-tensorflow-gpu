003f1f4b2a3c4b04f9f3f6903a3f03ef27d49218
Slavko <slavko@zitnik.si>
2018-09-14 16:00:18 +0200

    installation test

---
+++ installation_test.py
@@ -0,0 +1,11 @@
+# Python
+import tensorflow as tf
+hello = tf.constant('Hello, TensorFlow!')
+
+# Tensorflow porabi vse resurse, ki so na voljo, ne toliko, kolikor rabi oziroma kar je alocirano prek upravljavca nalog.
+# To popravimo tako, da ze pri vzpostavitvi seje dolocimo, da se rama ne alocira vnaprej:
+config = tf.ConfigProto()
+config.gpu_options.allow_growth = True
+
+sess = tf.Session(config= config)
+print(sess.run(hello))
